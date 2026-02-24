1c8da3366714ae19c4345da9aafe1c312fc6f8c7
kashefbasher <30617855+kashefbasher@users.noreply.github.com>
2026-02-24 15:37:07 -0500

    updated Main

--- myPractice/src/main/java/com/Main.java
+++ myPractice/src/main/java/com/Main.java
@@ -1,7 +1,39 @@
 package com;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static Integer[] convertIntArrayToIntegerArray(int[] intArray) {
+        Integer[] integerArray = new Integer[intArray.length];
+        for (int i = 0; i < intArray.length; i++) {
+            integerArray[i] = Integer.valueOf(intArray[i]);
+        }
+        return integerArray;
     }
+    static int mostFreqEle(int[] arr) {
+        Integer[] b = convertIntArrayToIntegerArray(arr);
+        Map<Integer, Long> map = Stream.of(b).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        Integer k=0;
+        Long v=0l;
+        for(Integer k1 : map.keySet()) {
+            Long v1 = map.get(k1);
+            if(v1>v) {
+                v=v1;
+                k=k1;
+            }
+        }
+        return  map.get(k).intValue();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 40, 50, 30, 40, 50, 30, 30 };
+        System.out.println(mostFreqEle(arr));
+    }
+
 }
